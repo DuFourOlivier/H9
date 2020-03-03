05eef934d444aa8dd20d001d334a97733823e1e6
Olivier <Olivier.dufour@student.ap.be>
2020-03-03 19:34:04 +0100

    pokemontester toegevoegd

---
+++ Pokemontester/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using pokémon;
+
+namespace Pokemontester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
