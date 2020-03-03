30a511700bf364a5584100dfd5f4cf0a4b88b6f2
Olivier <Olivier.dufour@student.ap.be>
2020-03-03 18:54:50 +0100

    Het maken+testen van enkele pokemon

--- pojemon/Program.cs
+++ pojemon/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using pokémon;
+using System;
 
 namespace pojemon
 {
@@ -6,7 +7,28 @@ namespace pojemon
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            pokemon Bulbasaur = new pokemon();
+            pokemon Charmander = new pokemon();
+            
+
+            Bulbasaur.HP_Base = 45;
+            Bulbasaur.Attack_Base = 49;
+            Bulbasaur.Defense_Base = 49;
+            Bulbasaur.SpecialAttack_Base = 65;
+            Bulbasaur.SpecialDefense_Base = 65;
+            Bulbasaur.Speed_Base = 45;
+           
+
+
+            Charmander.HP_Base = 39;
+            Charmander.Attack_Base = 52;
+            Charmander.Defense_Base = 43;
+            Charmander.SpecialAttack_Base = 60;
+            Charmander.SpecialDefense_Base = 50;
+            Charmander.Speed_Base = 65;
+
+            Console.WriteLine($"Bulbasaur heeft een average:{Bulbasaur.Average} en een total van {Bulbasaur.Total}");
+            Console.WriteLine($"Charmander heeft een average:{Charmander.Average} en een total van {Charmander.Total}");
         }
     }
 }
